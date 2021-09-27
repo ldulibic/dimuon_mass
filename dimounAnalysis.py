2d000fb3f53c47e0e6b2fa18599992d290634bee
ldulibic <lovro.dulibic@gmail.com>
2021-09-27 12:58:33 +0200

    radi sve do pozivanja hist

--- dimounAnalysis.py
+++ dimounAnalysis.py
@@ -11,7 +11,7 @@ df_os = df_2mu.Filter("Muon_charge[0] != Muon_charge[1]", "Munos with opposite c
 
 # c++ code to calculate invariant mass of the two muons
 ROOT.gInterpreter.Declare('''
-using Vec_t = const ROOT::RVec<float>&;
+using Vec_t = const ROOT::VecOps::RVec<float>&;
 float ComputeInvariantMass(Vec_t pt, Vec_t eta, Vec_t phi, Vec_t mass) {
     const ROOT::Math::PtEtaPhiMVector p1(pt[0], eta[0], phi[0], mass[0]);
     const ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], mass[1]);
